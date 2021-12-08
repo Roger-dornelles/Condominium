f80b9a2c61d08f66f2956ae4b95026f344843def
roger santos <rsd.santo@outlook.com>
2021-12-08 16:36:38 -0300

    Adicionado content-type ao headers

--- src/api/ApiConfig.js
+++ src/api/ApiConfig.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem('token');
 
 const api = axios.create({
   baseURL: 'https://api-condominium.herokuapp.com',
-  headers: {'Authorization': `Bearer ${token}`
+  headers: {'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json'
 }
 });
 export default api;
