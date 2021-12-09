b1ee01faad826c89595200910291687cef6b95bf
roger santos <rsd.santo@outlook.com>
2021-12-09 13:10:32 -0300

    removido barra do final do baseURL

--- src/api/ApiConfig.js
+++ src/api/ApiConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const api = axios.create({
-  baseURL: 'https://api-condominium.herokuapp.com/',
+  baseURL: 'https://api-condominium.herokuapp.com',
   headers: {'Authorization': `Bearer ${token}`}
 });
 export default api;
